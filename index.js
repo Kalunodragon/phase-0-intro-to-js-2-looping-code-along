522672029961773b68dedca3876dd5b38c41353a
Andrew <andrewonulak@gmail.com>
2022-10-15 20:07:02 -0600

    commit tree bd1dd41a3a2cc5aab30b5099ed69417847f583c6

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, events){
+    let tyc = []
+    for(let x = 0; x < names.length; x++){
+        tyc.push(`Thank you, ${names[x]}, for the wonderful ${events} gift!`)
+    }
+    return tyc
+}
+
+function countDown(x){
+    while (x >= 0){
+        console.log(x)
+        x--
+    }
+}
